c557baacba9180b8c8ab8828ba1c11fc6890b045
Nikhil <nikhilsamuel1404@gmail.com>
2021-12-14 06:00:09 -0500

    Added a data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import Todos from "./components/Todos";
+import Todo from "./models/Todo";
 
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn Typescript")];
   return (
     <div>
       <Todos items={["Learn React", "Learn Typescript"]} />
